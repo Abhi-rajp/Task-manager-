65c7359e121e5de8fc7b971cf5523d415db4d79f
Abhi-rajp <94036230+Abhi-rajp@users.noreply.github.com>
2025-11-26 12:14:50 +0530

    Create Taskform.js

---
+++ src/Taskform.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+function TaskForm({ addTask }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [priority, setPriority] = useState("Low");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title) return;
+
+    addTask({ title, description, priority });
+
+    setTitle("");
+    setDescription("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Task title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <textarea
+        placeholder="Description"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
+      <select value={priority} onChange={(e) => setPriority(e.target.value)}>
+        <option>Low</option>
+        <option>Medium</option>
+        <option>High</option>
+      </select>
+
+      <button type="submit">Add Task</button>
+    </form>
+  );
+}
+
+export default TaskForm;
